e7ed715b726d12e94b27e4187c2ec5e733d7d55b
joelanderson000 <joelanderson000@yahoo.ca>
2019-11-25 21:53:32 -0600

    add empty test for CI testing

---
+++ src/test/java/com/anderson/rest/server/ServerTest.java
@@ -0,0 +1,13 @@
+package com.anderson.rest.server;
+
+import org.junit.Test;
+
+
+public class ServerTest {
+
+
+    @Test
+    public void test() {
+        assert(true); //empty test for CI testing
+    }
+}
